bcb18312675616a2bcaac9a718e262073882aafb
abdennour touat <touatabdenour33@gmail.com>
2023-03-31 17:50:05 +0200

    🚀 feat(front-end): add mui-image and @types/mui-image to dependencies and devDependencies respectively The mui-image package has been added to the dependencies section of the package.json file. This package provides an image component for Material-UI. Additionally, the @types/mui-image package has been added to the devDependencies section to provide TypeScript type definitions for the mui-image package.
    🎨 style(ProductDetails.tsx): update product details page layout
    ✨ feat(ProductDetails.tsx): add image component and grid layout
    The product details page layout has been updated to display the product image on the left and the product details on the right. A new image component has been added to display the product image. The layout has been updated to use a grid layout to improve the page's responsiveness.

--- front-end/src/pages/ProductDetails.tsx
+++ front-end/src/pages/ProductDetails.tsx
@@ -4,22 +4,37 @@ import {
   CardContent,
   Typography,
   Button,
+  Box,
 } from "@mui/material";
+import Image from "mui-image";
 import { useStore } from "../Store";
 import { useProduct } from "../hooks/useProduct";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 export default function ProductDetails() {
   const addToCart = useStore((state) => state.addToCart);
   const product = useProduct();
+  // a product details page, the image on the left and the name, price rating description add to cart button and the quantity on the right\
+
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="300"
-        image={product.image}
-        alt={product.title}
-      />
-      <CardContent>
+    <Grid2 container spacing={8}>
+      <Grid2 md={8}>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          height="85vh"
+          overflow="hidden"
+        >
+          <Image
+            src={product.image}
+            alt={product.title}
+            height="80%"
+            fit="contain"
+          />
+        </Box>
+      </Grid2>
+      <Grid2 md={4}>
         <Typography variant="h5" component="h2">
           {product.title}
         </Typography>
@@ -36,9 +51,37 @@ export default function ProductDetails() {
         >
           Add to Cart
         </Button>
-      </CardContent>
-    </Card>
+      </Grid2>
+    </Grid2>
   );
+  // return (
+  //   <Card>
+  //     <CardMedia
+  //       component="img"
+  //       height="300"
+  //       image={product.image}
+  //       alt={product.title}
+  //     />
+  //     <CardContent>
+  //       <Typography variant="h5" component="h2">
+  //         {product.title}
+  //       </Typography>
+  //       <Typography variant="body1" color="textSecondary" component="p">
+  //         {product.description}
+  //       </Typography>
+  //       <Typography variant="h6" component="p">
+  //         Price: ${product.price}
+  //       </Typography>
+  //       <Button
+  //         variant="contained"
+  //         color="primary"
+  //         onClick={() => addToCart(product)}
+  //       >
+  //         Add to Cart
+  //       </Button>
+  //     </CardContent>
+  //   </Card>
+  // );
 }
 
 // export default ProductDetails;
