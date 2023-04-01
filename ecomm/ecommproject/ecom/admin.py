de8fc434257aa97df69e48259a9cae0fee658fb7
maycods <maya.houacine1@gmail.com>
2023-04-01 08:06:29 -1100

    admin + model

--- ecomm/ecommproject/ecom/admin.py
+++ ecomm/ecommproject/ecom/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from ecom.models import Produit,client,Images,Category,order,promotion,user 
+admin.site.register(Produit)
+admin.site.register(Category)
+admin.site.register(user)
+admin.site.register(client)
+admin.site.register(promotion)
+admin.site.register(Images)
+admin.site.register(order)
 
 # Register your models here.
--- ecomm/ecommproject/ecom/models.py
+++ ecomm/ecommproject/ecom/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Article (models.Model):
@@ -7,4 +8,38 @@ class Article (models.Model):
     registrationDate = models.DateField("Registration Date", auto_now_add=True)
     def __str__(self) -> str:
         return self.name
-
+##################################################################################   
+class Category (models.Model):
+    category= models.CharField( max_length=80)
+class promotion(models.Model):
+    promo=models.PositiveIntegerField(validators=[MinValueValidator(10), MaxValueValidator(70)])
+    titrep=models.CharField(max_length=30)
+    descp=models.CharField(max_length=80)
+    datedeb=models.DateField()
+    datefin=models.DateField()
+class Produit (models.Model):
+    title = models.CharField( max_length=80)
+    price = models.FloatField()
+    description: models.CharField( max_length=240)
+    categoryp = models.ForeignKey("Category",on_delete=models.SET_NULL,null=True,related_name='+',blank=True)
+    ratingt=models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    stock=models.PositiveIntegerField()
+    promotionref = models.ForeignKey("promotion",on_delete=models.SET_NULL,null=True,related_name='+',blank=True)
+    thumbnail= models.CharField( max_length=240)
+class Images (models.Model):
+    image=models.CharField( max_length=256)
+    prdRef=models.ForeignKey("Produit",on_delete=models.SET_NULL,null=True,related_name='+',blank=True)
+class user(models.Model):
+    username=models.CharField(max_length=15)
+    psw=models.CharField(max_length=60)
+class client(user):
+    name=models.CharField("Name", max_length=50) 
+    Telephone = models.PositiveIntegerField(validators=[MinValueValidator(100000), MaxValueValidator(999999)])
+    Mail = models.EmailField()#shoppingCard=#######
+class order (models.Model): 
+    produitref=models.ForeignKey("Produit",on_delete=models.SET_NULL,null=True,related_name='+',blank=True)
+    customerref=models.ForeignKey("client",on_delete=models.SET_NULL,null=True,related_name='+',blank=True)
+    qte= models.PositiveIntegerField(validators=[MinValueValidator(1)])#max value is the store attribue in produit   
+    rating=models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    prixtotal= models.FloatField("price")#attribut calculé    
+    unique_together = ('produitref','customerref')
